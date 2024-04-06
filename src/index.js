2272fc09aa37884b717d7f07fe90c9172bf61ee8
aus-0-tin <austin.waithaka@student.moringaschool.com>
2024-04-06 17:17:16 +0300

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskDescription = taskInput.value;
+n
+    const newTask = document.createElement("li");
+    newTask.textContent = taskDescription;
+
+    const taskList = document.getElementById("tasks");
+    taskList.appendChild(newTask);
+
+    taskInput.value = "";
+
+  });
+
 });
+
